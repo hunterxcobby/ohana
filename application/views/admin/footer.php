cf5c94279272268598c0931ae6af2b6f0c16720a
hunterxcobby <solomonsefah13@gmail.com>
2024-06-03 11:16:39 +0000

    `Update footer.php to remove copyright year and company name`

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -3,7 +3,7 @@
 		<div class="footer-content">
 			<span class="bigger-120">
 				<span class="blue bolder"> Restaurant Management System </span>
-				 &copy; 2017-<?=date('Y')?> by Red Planet Computers Pvt Ltd
+				 <!-- &copy; 2017-<?=date('Y')?> by Red Planet Computers Pvt Ltd -->
 			</span>
 
 			&nbsp; &nbsp;
